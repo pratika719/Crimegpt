848fa84d2c300a16eb552f6e6c39357e2336980d
pratika719 <pratikagrawal719@gmail.com>
2026-06-25 16:22:26 +0530

    fix onnxruntime binary exclusion glob pattern

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,8 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["@huggingface/transformers", "onnxruntime-node"],
   outputFileTracingExcludes: {
     "*": [
-      "./node_modules/onnxruntime-node/bin/napi-v6/win32/**/*",
-      "./node_modules/onnxruntime-node/bin/napi-v6/darwin/**/*",
-      "./node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**/*",
+      "node_modules/onnxruntime-node/bin/**/*",
+      "!node_modules/onnxruntime-node/bin/napi-v6/linux/x64/**/*",
     ],
   },
   async headers() {
